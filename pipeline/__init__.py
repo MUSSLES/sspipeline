e3f6743935831db761150ac546afc2bca9e453c6
John Letey <john.letey@colorado.edu>
2018-08-17 14:54:09 -0600

    added some code

---
+++ pipeline/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: John Letey, University of Colorado, Boulder
+
+pipeline package init file.
+
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+__version__ = "0.01"
+__author__ = "John Letey (john.letey@colorado.edu)"
+__copyright__ = "Copyright 2018 MUSSLES"
+
+from .distributions import *
