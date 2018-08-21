25a68db642fe7f9c82b79feb3358ad9f630c1538
John Letey <john.letey@colorado.edu>
2018-08-21 12:18:29 -0600

    added code of conduct, updated latex paper, trying to fix travis error

--- setup.py
+++ setup.py
@@ -35,40 +35,22 @@ if __name__ == "__main__":
         license=LICENSE,
         packages=setuptools.find_packages(),
         entry_points={"console_scripts": ["pipeline=pipeline:cli_main"]},
-        install_requires=[
-            'click',
-            'numpy',
-            'pandas',
-            'scipy',
-            'matplotlib'
-        ],
+        install_requires=["click", "numpy", "pandas", "scipy", "matplotlib"],
         extras_require={
-            'docs': [
-                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
-                'sphinxcontrib-napoleon',
-                'sphinx_rtd_theme',
-                'numpydoc',
+            "docs": [
+                "sphinx==1.2.3",  # autodoc was broken in 1.3.1
+                "sphinxcontrib-napoleon",
+                "sphinx_rtd_theme",
+                "numpydoc",
             ],
-            'tests': [
-                'pytest',
-                'pytest-cov',
-                'pytest-pep8',
-                'flake8'
-            ],
+            "tests": ["pytest", "pytest-cov", "pytest-pep8", "flake8"],
         },
-
-        tests_require=[
-            'pytest',
-            'pytest-cov',
-            'pytest-pep8',
-            'flake8'
-        ],
-
+        tests_require=["pytest", "pytest-cov", "pytest-pep8", "flake8"],
         classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
         ],
         zip_safe=True,
         long_description=long_description,
